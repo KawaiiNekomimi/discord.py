9a307ce75971df159eeef82dd1a4cd3ad5ecd5d5
KawaiiNekomimi <57449290+KawaiiNekomimi@users.noreply.github.com>
2019-12-13 09:05:55 -0700

    Update to latest version, fix english errors

--- Standalone Version/shion.py
+++ Standalone Version/shion.py
@@ -1,13 +1,10 @@
-import datetime
-import json
 import os
 import re
-import time
-import shion_lib
-import discord
-import requests
 import sys
 
+import discord
+
+import shion_lib
 
 class MyClient(discord.Client):
 
@@ -15,96 +12,125 @@ class MyClient(discord.Client):
         await client.change_presence(activity=discord.Game(name='!sbhelp'))
 
     async def on_message(self, message):
-        path = os.path.dirname(os.path.abspath(__file__))
         if message.author == self.user:
             return
-        blacklist = []
-        if message.author.id in blacklist:
+
+        if re.compile(r"!soul").match(message.content):
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
+            try:
+                await shion_lib.get_soul_count(message)
+            except ValueError:
+                await shion_lib.message_profile_not_found(message)
+            except FileNotFoundError:
+                await shion_lib.message_userdata_not_found(message)
             return
 
         if re.compile(r"!ah").match(message.content):
-            author = str(message.author.id)
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
             try:
-                usrdata = path + "/usrdata/" + author +".json"
-                f = open(usrdata)
-                usr = json.load(f)
-                name = str(usr["name"])
-                profile_name = str(usr["profile_name"])
-                profile_id = str(usr["profile_id"])
-                embed = shion_lib.get_myauction(name, profile_id)
-                await message.channel.send(message.author.mention,embed=embed)
+                await shion_lib.get_my_auction(message)
             except ValueError:
-                await message.channel.send(message.author.mention + shion_lib.ValueErrorMessage(profile_name))
+                await shion_lib.message_profile_not_found(message)
             except FileNotFoundError:
-                await message.channel.send(message.author.mention + shion_lib.FileNotFoundErrorMessage())
+                await shion_lib.message_userdata_not_found(message)
             return
 
-        if re.compile(r"(!add )(.+)( |,)(.+)").match(message.content):
-            author = str(message.author.id)
-            com = re.match("(!add )(.+)( |,)(.+)", message.content)
-            name = com.group(2)
-            profile_name = com.group(4).capitalize()
+        if re.compile(r"(!add )(.+)( )(.+)").match(message.content):
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
             try:
-                shion_lib.addnewusr(author, name, profile_name)
-                await message.channel.send(f"{message.author.mention} Registration Completed.\nMinecraft Name: {name}\nProfile: {profile_name}")
+                await shion_lib.add_new_user(message)
             except ValueError:
-                await message.channel.send(shion_lib.ValueErrorMessage(profile_name))
-                
+                await shion_lib.message_profile_not_found(message)
+
         if re.compile(r"!minion").match(message.content):
-            author = str(message.author.id)
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
             try:
-                usrdata = path + "/usrdata/" + author +".json"
-                f = open(usrdata)
-                usr = json.load(f)
-                name = str(usr["name"])
-                profile_id = str(usr["profile_id"])
-                embed = shion_lib.get_minions_unlock(name, profile_id)
-                await message.channel.send(message.author.mention,embed=embed)
+                await shion_lib.get_minions_unlock(message)
             except ValueError:
-                await message.channel.send(shion_lib.ValueErrorMessage(profile_name))
+                await shion_lib.message_profile_not_found(message)
             except FileNotFoundError:
-                await message.channel.send(message.author.mention + shion_lib.FileNotFoundErrorMessage())
+                await shion_lib.message_userdata_not_found(message)
 
-        event_channels = []
-        if message.author == self.user:
-            return
-        if message.channel.id in event_channels:
+
+        unpublished_channels = shion_lib.unpublished_channels
+        if message.channel.id in unpublished_channels:
             if re.match(r"(!newyear|!ny)", message.content):
-                result = shion_lib.get_event("New Years Event", "https://hypixel-api.inventivetalent.org/api/skyblock/newyear/estimate")
+                result = shion_lib.get_event("New Years Event",
+                                             "https://hypixel-api.inventivetalent.org/api/skyblock/newyear/estimate")
                 await message.channel.send(message.author.mention + result)
             elif re.match(r"(!spooky|!sp)", message.content):
-                result = shion_lib.get_event("Spooky Festival", "https://hypixel-api.inventivetalent.org/api/skyblock/spookyFestival/estimate")
+                result = shion_lib.get_event("Spooky Festival",
+                                             "https://hypixel-api.inventivetalent.org/api/skyblock/spookyFestival/estimate")
                 await message.channel.send(message.author.mention + result)
             elif re.match(r"(!magmaboss|!mb)", message.content):
-                result = shion_lib.get_event("Magma Boss Spawn", "https://hypixel-api.inventivetalent.org/api/skyblock/bosstimer/magma/estimatedSpawn")
+                result = shion_lib.get_event("Magma Boss Spawn",
+                                             "https://hypixel-api.inventivetalent.org/api/skyblock/bosstimer/magma/estimatedSpawn")
                 await message.channel.send(message.author.mention + result)
             elif re.match(r"(!darkah|!da)", message.content):
-                result = shion_lib.get_event("Dark Auction", "https://hypixel-api.inventivetalent.org/api/skyblock/darkauction/estimate")
+                result = shion_lib.get_event("Dark Auction",
+                                             "https://hypixel-api.inventivetalent.org/api/skyblock/darkauction/estimate")
                 await message.channel.send(message.author.mention + result)
             elif re.match(r"!bank", message.content):
-                result = shion_lib.get_event("Bank Interest", "https://hypixel-api.inventivetalent.org/api/skyblock/bank/interest/estimate")
+                result = shion_lib.get_event("Bank Interest",
+                                             "https://hypixel-api.inventivetalent.org/api/skyblock/bank/interest/estimate")
                 await message.channel.send(message.author.mention + result)
             else:
                 pass
-        
+
+        if re.compile(r"(!seen )(.+)").match(message.content):
+            await shion_lib.get_player_session(message)
+
         if re.compile(r"!news").match(message.content):
-            embed = shion_lib.get_sb_news()
-            await message.channel.send(embed = embed)
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
+            await shion_lib.get_sb_news(message)
+
+        if re.compile(r"!patch|!jerry|!update").match(message.content):
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
+            await shion_lib.get_sb_patch(message)
 
         if re.compile(r"!sbhelp").match(message.content):
-            embed = shion_lib.help()
-            await message.channel.send(message.author.mention, embed=embed)
-            
+            if shion_lib.check_blacklist(message):
+                await shion_lib.message_in_blacklist(message)
+                return
+            await shion_lib.sb_help(message)
+
+        if re.compile(r"(!addblacklist )(.+)( )(.+)").match(message.content):  #This command is needed 2 args Discord users.id and reason.
+            author_id = message.author.id  #(you shouldn't use blank in reason. For ex. "!addblacklist 00000000 spam_chat")
+            if not author_id == owner_id:
+                return
+            await shion_lib.add_blacklist(message)
+        
+        global_notice = shion_lib.global_notice_from
+        notice = shion_lib.global_notice_to
+        if message.channel.id == global_notice:
+            for channel in notice:
+                notice_channel = client.get_channel(channel)
+                if not notice_channel == None:
+                    await notice_channel.send(message.content)
+        
+        #Change bot's code on during it run and use this command then bot will restart itself. Special Thanks: kawaiinekomimi
         if re.compile(r"!reload").match(message.content):
             #Basic version, will be improved in the future.
-            author_id = str(message.author.id)
-            if author_id != owner_id:
+            author_id = message.author.id
+            if not author_id == owner_id:
                 return
             await message.channel.send(":white_check_mark: Reloaded!")
             os.execl(sys.executable, sys.executable, * sys.argv)
 
-owner_id = "Bot owner ID"
+
+owner_id = shion_lib.owner_id
 client = MyClient()
-token = ("TOKEN")
+token = shion_lib.token
 client.run(token)
-path = os.path.dirname(os.path.abspath(__file__))
